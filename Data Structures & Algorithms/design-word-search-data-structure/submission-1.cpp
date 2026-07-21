556aec1d552d4d8054f094d410f3d59f0c454639
Nakshatra Jain <nakshatrajain246@gmail.com>
2026-07-21 13:51:59 +0530

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.cpp
@@ -0,0 +1,60 @@
+class WordDictionary {
+private:
+    class TrieNode {
+    public:
+        vector<TrieNode*> children;
+        bool isEnd;
+
+        TrieNode(){
+            children = vector<TrieNode*>(26, nullptr);
+            isEnd = false;
+        }
+
+        ~TrieNode(){
+            for(auto child : children){
+                if(child) delete child;
+            }
+        }
+    };
+
+    TrieNode* node;
+public:
+    WordDictionary() {
+        node = new TrieNode();
+    }
+    
+    void addWord(string word) {
+        TrieNode* tmp = node;
+        for(char c: word){
+            if(!tmp->children[c-'a']) tmp->children[c-'a'] = new TrieNode();
+            tmp= tmp->children[c-'a'];
+        }
+        tmp->isEnd=true;
+    }
+    
+    bool search(string word) {
+        TrieNode* tmp = node;
+        return searchHelper(word, 0, tmp);
+    }
+
+    bool searchHelper(const string& word, int index, TrieNode* currNode){
+        if(index==word.size()) return currNode->isEnd;
+        for(int i=index; i<word.size();i++){
+            char c = word[i];
+            if(c=='.'){
+                bool found=false;
+                for(auto child: currNode->children){
+                    if(child){
+                        found=found || searchHelper(word, i+1, child);
+                    }
+                    if(found) return found;
+                }
+                return found;
+            } else {
+                if(!currNode->children[c-'a']) return false;
+                currNode=currNode->children[c-'a'];
+            }
+        }
+        return currNode->isEnd;
+    }
+};
